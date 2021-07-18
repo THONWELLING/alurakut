320bf93bb03fa22773aae70cc8e27851eda62a85
THONWELLING <thonwellingdani13@gmail.com>
2021-07-18 03:48:09 -0300

    primeiro comit imersão React

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import {AlurakutStyles} from '../src/lib/alurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  body {
+    background-color: #09E6F6;
+    font-family: sans-serif;
+  }
+  #_next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  ${AlurakutStyles}
 `
 
 const theme = {
